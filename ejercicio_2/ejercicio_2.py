ddcce98bc63e5146c181a82c16de7360cb95c94f
sistemas <you@example.com>
2023-03-08 09:18:23 -0500

    se modifico ejercicio_2.py

---
+++ ejercicio_2/ejercicio_2.py
@@ -0,0 +1,17 @@
+# programa para realizar un prestamo bancario
+
+# input 
+ing = int(input("digite sus ingresos: "))
+deudas = int(input("digite sus deudas: "))
+
+# processing
+if ing > 945200:
+    if deudas == 0:
+        print ("prestamo aprobado")
+else:
+   ing < 945200
+   deudas > 0
+   print ("prestamo no aprobado")
+
+#output
+print 
