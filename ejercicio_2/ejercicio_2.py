13892c69e3a40fc962fd69926252835e011cae3a
sistemas <you@example.com>
2023-03-09 14:34:08 -0500

    se agrego el diagrama de flujo y se modifico el ejercicio_2.py

--- ejercicio_2/ejercicio_2.py
+++ ejercicio_2/ejercicio_2.py
@@ -6,10 +6,10 @@ deudas = int(input("digite sus deudas: "))
 
 # processing
 if ing > 945200:
+    
     if deudas == 0:
         print ("prestamo aprobado")
 else:
-   ing < 945200
    deudas > 0
    print ("prestamo no aprobado")
 
